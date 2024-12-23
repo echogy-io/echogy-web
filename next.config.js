21338d84b4760f1413efca702855870b9d55776a
youkale <tdycxs@gmail.com>
2024-12-23 17:49:04 +0800

    feat: update cf config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+const { setupDevPlatform } = require('@cloudflare/next-on-pages/next-dev');
+
 const nextConfig = {
   /* config options here */
 };
+
+if (process.env.NODE_ENV === 'development') {
+  setupDevPlatform();
+}
 
 module.exports = nextConfig;
